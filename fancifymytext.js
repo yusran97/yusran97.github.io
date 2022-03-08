dbd1692bc0e175bb3ba44aaf3a274e36ea4b5b67
yusran97 <101157443+yusran97@users.noreply.github.com>
2022-03-07 18:07:47 -0800

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,32 @@
+
+
+//bigger
+function bigz() {
+   var text = document.getElementById("feedme");
+   text.style.fontSize = "24pt";
+}
+
+//bold
+function boldz() {
+   var text = document.getElementById("feedme");
+   text.style.fontWeight = "bold";
+   text.style.color= "blue";
+   text.style.textDecoration= "underline";
+}
+
+//unbold
+function boringz() {
+   var text = document.getElementById("feedme");
+   text.style.fontSize = "initial";
+   text.style.fontWeight = "normal";
+   text.style.color = "black";
+   text.style.textDecoration= "none";
+}
+
+//uppercase+ -Moo suffix
+function mooz() {
+   var suffix = '-Moo',
+   txt = document.getElementById('feedme');
+   txt.style.textTransform="uppercase";
+   txt.value = txt.value.split('.').join('') + suffix;
+}
